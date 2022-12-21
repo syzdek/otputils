1f3fc88d358d8b7479125729df3e3fc97eb5d445
David M. Syzdek <david@syzdek.net>
2022-12-21 11:24:02 -0900

    libtotputils: adding totputils_bv_t

--- include/totputils.h
+++ include/totputils.h
@@ -117,6 +117,13 @@
 /// TOTP Utils descriptor state
 typedef struct totp_utils TOTPUtils;
 
+typedef struct totputils_berval totputils_bv_t;
+struct totputils_berval
+{
+   size_t         bv_len;
+   void *         bv_val;
+};
+
 
 //////////////////
 //              //
