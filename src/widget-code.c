cf67591a2377259e9a8d19aad1fd42a697b8102e
David M. Syzdek <david@syzdek.net>
2022-12-27 12:28:13 -0900

    totp: resolving xcode analyzer warnings

--- src/widget-code.c
+++ src/widget-code.c
@@ -106,10 +106,10 @@ totp_widget_code(
    if ((otp_method == TOTPUTILS_TOTP) && (!(cnf->quiet)))
    {
       snprintf(totp_tx_str, sizeof(totp_tx_str), "%" PRId64, totp_tx);
-      printf("%s (%*" PRId64 "s/%" PRId64 "s)\n", buff, (int)strlen(totp_tx_str), totputils_totp_timer(cnf->tud, 0), totp_tx);
+      printf("%s (%*" PRId64 "s/%" PRId64 "s)\n", code, (int)strlen(totp_tx_str), totputils_totp_timer(cnf->tud, 0), totp_tx);
    } else
    {
-      printf("%s\n", buff);
+      printf("%s\n", code);
    };
 
    return(0);
